e39818ab66be180afc3e1770b785ee9060ea6651
a-davlatov <davlatovamriddin7@gmail.com>
2025-03-13 11:47:56 +0500

    remove 3D scene for mobile devices

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -5,6 +5,7 @@ import { onMounted } from 'vue'
 import HomeSceneSneaker from '@/components/blocks/home/HomeSceneSneaker.vue'
 import HomeSwiperIntro from '@/components/blocks/home/HomeSwiperIntro.vue'
 import HomeSwiperProducts from '@/components/blocks/home/HomeSwiperProducts.vue'
+const isMobile = window.innerWidth <= 768
 
 onMounted(() => {
   if (!WebGL.isWebGL2Available()) console.warn('Warning : Your graphics card does not seem to support WebGL 2')
@@ -12,7 +13,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <HomeSceneSneaker v-if="WebGL.isWebGL2Available() && window.innerWidth > 768" />
+  <HomeSceneSneaker v-if="WebGL.isWebGL2Available() && !isMobile" />
 
   <HomeSwiperIntro v-else />
 
