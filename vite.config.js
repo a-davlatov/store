eb3858836c3d30fd5ab689bce134f41a86374da3
a-davlatov <davlatovamriddin7@gmail.com>
2025-03-16 17:35:03 +0500

    add file compression

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteCompression({
+      verbose: true, // Показывает сжатые файлы в консоли
+      disable: false, // Отключает плагин (по умолчанию false)
+      threshold: 10240, // Минимальный размер файла для сжатия (10 KB)
+      algorithm: 'gzip', // Используемый алгоритм (gzip, brotliCompress, deflate, deflateRaw)
+      ext: '.gz', // Расширение сжатых файлов
+    }),
   ],
   resolve: {
     alias: {
